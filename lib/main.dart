138385f639c3f787f6a0b699927209738e0f1b6d
Raju KC <raju.kc3@gmail.com>
2021-06-15 15:31:41 +0545

    todolist screen page created

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:todoapp/screens/todo_list_screen.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      title: 'My TODO List',
+      home: TodoListScreen(),
+    );
   }
 }
